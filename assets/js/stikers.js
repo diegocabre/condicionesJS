532afe370debdaeadaef288059e981cc5ecfc154
Diego Cabre <diegocabre@gmail.com>
2023-07-11 23:28:53 -0400

    quitar lso comentarios del stikers

--- assets/js/stikers.js
+++ assets/js/stikers.js
@@ -5,17 +5,6 @@ function calcular() {
     const stikers3 = parseInt(document.getElementById("numero3").value);
     const parrafo = document.getElementById("parrafo__stikers");
     const suma = stikers1 + stikers2 + stikers3;
-
-    // if (stikers1 = a) {
-    //     const total = (stikers1 + suma) - suma;
-    //     parrafo.innerHTML = "Llevas : " + total;
-    // } else if (stikers2 <= a) {
-    //     const total = (stikers2 + suma) - suma;
-    //     parrafo.innerHTML = "Llevas : " + total;
-    // } else if (stikers3 <= a) {
-    //     const total = (stikers3 + suma) - suma;
-    //     parrafo.innerHTML = "Llevas : " + total;
-    // } else 
     if (stikers1 < 0 || stikers2 < 0 || stikers3 < 0) {
         alert("El numero ingresado debe ser mayor o igual de 0");
     } else if (suma <= a) {
